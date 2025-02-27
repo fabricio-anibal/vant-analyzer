719306df2a0693a87d5fd7c1108cd19a0e1e9f89
Fabricio Anibal <fabricio_anibal@hotmail.com>
2025-02-26 23:58:30 -0300

    refact

--- vant-analizer.py
+++ vant-analizer.py
@@ -19,31 +19,25 @@ def cria_grafo():
     # Criar um grafo
     G = nx.Graph()
 
-    # Adicionar nós com coordenadas 3D
     nodes = {i: (np.random.rand(), np.random.rand(), np.random.rand()) for i in range(1, 6)}
     G.add_nodes_from(nodes.keys())
 
-    # Adicionar arestas aleatórias
     edges = [(1, 2), (1, 3), (2, 4), (3, 5), (4, 5)]
     G.add_edges_from(edges)
 
-    # Criar a figura 3D
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection='3d')
 
-    # Desenhar nós
     for node, (x, y, z) in nodes.items():
         ax.scatter(x, y, z, color='lightblue', s=200)
         ax.text(x, y, z, str(node), color='black', fontsize=12, ha='center')
 
-    # Desenhar arestas
     for edge in edges:
         x_vals = [nodes[edge[0]][0], nodes[edge[1]][0]]
         y_vals = [nodes[edge[0]][1], nodes[edge[1]][1]]
         z_vals = [nodes[edge[0]][2], nodes[edge[1]][2]]
         ax.plot(x_vals, y_vals, z_vals, color='gray')
 
-    # Configuração dos eixos
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
@@ -56,16 +50,13 @@ class Graph:
         self.edges = edges
 
     def draw(self):
-        # Criar a figura 3D
         fig = plt.figure(figsize=(8, 6))
         ax = fig.add_subplot(111, projection='3d')
 
-        # Desenhar nós
         for node, (x, y, z) in self.nodes:
             ax.scatter(x, y, z, color='lightblue', s=200)
             ax.text(x, y, z, str(node), color='black', fontsize=12, ha='center')
 
-        # Desenhar arestas
         nodes_dict = {node: (x, y, z) for node, (x, y, z) in self.nodes}
 
         for edge in self.edges:
@@ -74,7 +65,6 @@ class Graph:
             z_vals = [nodes_dict[edge[0]][2], nodes_dict[edge[1]][2]]
             ax.plot(x_vals, y_vals, z_vals, color='gray')
 
-        # Configuração dos eixos
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
